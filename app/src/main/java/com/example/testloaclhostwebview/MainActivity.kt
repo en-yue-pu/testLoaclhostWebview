82c757f1fc04d6c35fdb11bb796af11040b3d472
yue_pu <yue_pu@en-japan.com>
2023-04-24 10:47:07 +0900

    remove

--- app/src/main/java/com/example/testloaclhostwebview/MainActivity.kt
+++ app/src/main/java/com/example/testloaclhostwebview/MainActivity.kt
@@ -30,12 +30,5 @@ class MainActivity : AppCompatActivity() {
         //http://10.0.2.2 http://www.google.co.jp  http://10.0.2.2:8080
         webView.loadUrl("http://10.0.2.2:8080")
 
-        webView.webViewClient = object : WebViewClient() {
-            @Deprecated("Deprecated in Java")
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                view.loadUrl(url)
-                return true
-            }
-        }
     }
 }
